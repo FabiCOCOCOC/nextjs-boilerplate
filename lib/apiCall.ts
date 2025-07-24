7451efffe59dc3913e1dbb84e1e83d0c31873dd2
FS.SAnter <fsanter@student.tugraz.at>
2025-07-24 15:41:18 +0200

    Plain setup

---
+++ lib/apiCall.ts
@@ -0,0 +1,6 @@
+// This is a placeholder file for API calls
+// You can add your own functions here
+
+export const placeholder = () => {
+  console.log("Add your API functions here");
+};
---
+++ lib/utils.ts
@@ -0,0 +1,6 @@
+// Utility functions placeholder
+// Add your utility functions here
+
+export const placeholder = () => {
+  console.log("Add your utility functions here");
+};
