3f8fb1bceaefad2e509421f4800459974212da37
FS.SAnter <fsanter@student.tugraz.at>
2025-08-07 16:37:10 +0200

    Added plotting func which displays monthly per default

--- app/stockInfo/[symbol]/page.tsx
+++ app/stockInfo/[symbol]/page.tsx
@@ -1,5 +1,6 @@
 //Shows the infos of the designated stock!
 import yfinance from 'yahoo-finance2';
+import StockChart from '@/lib/plotting';
 //import ApiTest from '@/tests/ApiTest';
 
 
@@ -13,12 +14,25 @@ export default async function StockInfoPage({ params }: StockInfoPageProps) {
     const { symbol } = await params;
     const stockData = await yfinance.quote(symbol);
 
+    const historical = await yfinance.historical(symbol, {
+        period1: new Date(Date.now() - 12 * 30 * 24 * 60 * 60 * 1000),
+        period2: new Date(),
+        interval: '1mo',
+    });
+
+    const historicalData = historical.map((data) => ({
+        dates: data.date.toLocaleDateString('en-GB', {
+            month: 'short'
+        }),
+        value: data.close,
+    }));
+
     return (
-        <div>
+    <div>
     <div className="bg-gray-200 min-h-screen flex flex-col items-center pt-8">
         <div className="bg-white w-full max-w-4xl min-h-[250px] p-10 rounded-lg shadow-lg">
                 <div className="mb-6">
-                    <h1 className="text-lg font-bold mb-0 text-left">{stockData.longName?.toUpperCase()}</h1>
+                    <h1 className="text-lg font-bold mb-0 text-left">{stockData.longName?.toUpperCase()} {stockData.fullExchangeName}</h1>
                     <p className="pt-8">
                         <span className="text-3xl">{stockData?.regularMarketPrice}</span>
                         <span className="text-gray-600 mr-20">{stockData?.currency}</span>
@@ -28,9 +42,18 @@ export default async function StockInfoPage({ params }: StockInfoPageProps) {
                                 <span className="mr-20">{stockData.currency}</span>
                             </span>
                         ) : null}
+                        {typeof stockData?.regularMarketChangePercent === 'number' ? (
+                        <span className={stockData.regularMarketChangePercent < 0 ? 'text-red-500' : 'text-green-500'}>
+                                <span className="text-3xl"> {stockData.regularMarketChangePercent < 0 ? "" : "+"}{stockData.regularMarketChangePercent}</span>
+                                <span className="mr-20">%</span>
+                        </span>
+                        ) : null}
                     </p>
                 </div>
             </div>
+                <div className = "mt-8">    {/* distance between the chart and the infos  */}
+                    <StockChart data={historicalData} />
+                </div>
         </div>
     </div>
 
---
+++ lib/plotting.tsx
@@ -0,0 +1,49 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+  Label,
+} from 'recharts';
+
+interface StockChartData {
+  dates: string;
+  value: number;
+}
+
+interface StockChartProps {
+  data: StockChartData[];
+}
+
+export default function StockChart({ data }: StockChartProps) {
+  return (
+    <div className="bg-white w-full max-w-screen-xl min-w-[800px] min-h-[650px] p-10 rounded-lg shadow-lg">
+      <h3 className="text-lg font-semibold mb-4"> -chart</h3>
+      <div className="w-full h-64">
+      <ResponsiveContainer width={500} height={500}>
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis 
+        dataKey="dates" 
+        angle = {-45}
+        textAnchor="end"
+        height={80}
+        interval = "preserveStartEnd"
+        />
+        <YAxis />
+        <Tooltip 
+          formatter={(value) => [`Value: ${value}`, 'USD']}
+        />
+        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      </LineChart>
+    </ResponsiveContainer>    
+    </div>
+    </div>
+  );
+}
